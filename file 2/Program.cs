8eaa6256f2558371e33ec00f6675ca097711d1ac
Pavel Sawchenko <pashka.sawchenko@yandex.ru>
2022-11-10 08:51:04 +0300

    feat: new file

---
+++ file 2/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите несколько чисел: ");
+int[] numbers = Console.ReadLine().Split(new char[]{' '}, StringSplitOptions.RemoveEmptyEntries).Select(i=>int.Parse(i)).ToArray<int>();;
+
+void PrintArray(int[] numbers)
+{
+    int result = 0;
+    int = numbers.Length;
+    while (i < count)
+    {
+        Console.Write($"[{numbers[i]}]");
+        i++;
+    }
+}
+int[] array = new int[numbers.Length];
+PrintArray(numbers);
+int result = 0;
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > 0) count++;
+}
+Console.WriteLine();
+Console.WriteLine($"Колличество цифр больше нуля = {count}");
